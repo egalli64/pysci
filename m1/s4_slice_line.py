2d3831962c7753be7dca4b51bb7832fc98e68bdd
Emanuele <egalli64@gmail.com>
2023-12-15 17:36:17 +0100

    no matrix

--- m1/s4_slice_line.py
+++ m1/s4_slice_line.py
@@ -32,7 +32,3 @@ print("Reversed:", reversed)
 
 reversed_alt = a[::-2]
 print("Reversed alternate:", reversed_alt)
-
-# multi dim
-m = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 0, 1, 2]])
-print("A matrix:\n", m)
