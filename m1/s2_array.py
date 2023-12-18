29a5af2c8327f6fe15937bdc6365e4382cbcf6a3
Emanuele <egalli64@gmail.com>
2023-12-18 18:20:53 +0100

    move the checker in other module

--- m1/s2_array.py
+++ m1/s2_array.py
@@ -8,43 +8,42 @@ Module 1 - NumPy
 The ndarray
 """
 import numpy as np
+from s2_ndarray_checker import check
 
-print("Introducing NumPy version", np.__version__)
+print("Using NumPy version", np.__version__)
 
 # a native list (of anything)
-list = [x for x in range(3)]
-list.append("42")
-print("A native Python list:", list)
+list = [x for x in range(10)]
+list[-1] = "42"
+print(f"{list} is an object of type {type(list)}")
 
 if isinstance(list[-1], str):
-    # example with native array
+    # Python native array
     from array import array
 
     # arrays are homogeneous, comment next line to get a TypeError
-    del list[-1]
+    list[-1] = 9
 
     # a native int array
     native_array = array("i", list)
-    print("A native Python array:", native_array)
+    print(f"{native_array} is an object of type {type(native_array)}")
 
-# NumPy integer ndarray (homogeneous)
-a = np.array([1, 2, 3])
-print(f"An {a.dtype} {type(a)} sized {a.size}: {a}")
 
-# NumPy tries to understand which type use for array
-a2 = np.array([1, 2, 10 / 3])
-print(f"A {a2.dtype} {type(a2)}: {a2}")
+# NumPy unicode-32 string ndarray (forced to be homogeneous)
+list[-1] = "hello"
+check(np.array(list))
 
-# requiring a base type
-a3 = np.array([1, 2, 3], dtype=float)
-print(f"Another {a3.dtype} {type(a3)}: {a3}")
+# NumPy integer ndarray (inherently homogeneous)
+list[-1] = 9
+check(np.array(list))
 
-# matrices
-m1 = np.array([range(i, i + 3) for i in range(3)])
-print(f"An {m1.dtype} {type(m1)} matrix shaped {m1.shape}:\n", m1)
+# NumPy float64 ndarray (forced to be homogeneous)
+check(np.array(np.array([1, 2, 10 / 3])))
 
-m2 = np.array([[1, 2], [3, 4], [5, 6]])
-print(f"An {m2.dtype} {type(m2)} matrix shaped {m2.shape}:\n", m2)
+# 2-dimensional arrays
+check(np.array([range(i, i + 3) for i in range(2)]))
+check(np.array([[1, 2], [3, 4], [5, 6]]))
 
-m3 = np.array([[1, 2, 3], [4, 5, 6]], dtype=complex)
-print(f"A {m3.dtype} {type(m3)} matrix shaped {m3.shape}:\n", m3)
+# forcing the dtype by argument
+check(np.array([1, 2, 3], dtype=float))
+check(np.array([[1, 2, 3], [4, 5, 6]], dtype=complex))
