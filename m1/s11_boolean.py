d77be429010414a9dd2617a829b23ed0a9582447
Emanuele <egalli64@gmail.com>
2023-12-18 10:14:37 +0100

    mask

--- m1/s11_boolean.py
+++ m1/s11_boolean.py
@@ -31,3 +31,7 @@ print("How many m < 5 in each row?", np.sum(m < 5, axis=1))
 print("Any value in a < 5?", np.any(a < 5))
 print("All values in m >= 3?", np.all(m >= 3))
 print("All values in m > 1 (by row)?", np.all(m > 1, axis=1))
+
+# mask
+print("Show a > 5", a[a > 5])
+print("Show a <= 5", a[a <= 5])
