fb55e3b5668a974bcad48fce58ec6d2fca3800c7
Emanuele <egalli64@gmail.com>
2023-12-21 20:46:19 +0100

    numpy interp

---
+++ m2/s4_interpolate/e1.interp.py
@@ -0,0 +1,23 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 2 - SciPy
+
+Interpolation - 1-D - numpy.interp
+"""
+import numpy as np
+
+x = np.linspace(0, 10, num=11)
+y = np.cos(-(x**2) / 9.0)
+
+for p in zip(x, y):
+    print(p)
+print()
+
+x_in = np.linspace(0, 10, num=1001)
+y_in = np.interp(x_in, x, y)
+
+for i in range(50, 1001, 100):
+    print(x_in[i], y_in[i])
