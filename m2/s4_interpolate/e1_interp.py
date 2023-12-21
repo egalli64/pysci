20af6ddb529aeafbb6379c3815b95a5d91b1afb7
Emanuele <egalli64@gmail.com>
2023-12-21 21:07:11 +0100

    minor review

--- m2/s4_interpolate/e1_interp.py
+++ m2/s4_interpolate/e1_interp.py
@@ -12,6 +12,7 @@ import numpy as np
 x = np.linspace(0, 10, num=11)
 y = np.cos(-(x**2) / 9.0)
 
+print("The passed points:")
 for p in zip(x, y):
     print(p)
 print()
@@ -19,5 +20,6 @@ print()
 x_in = np.linspace(0, 10, num=1001)
 y_in = np.interp(x_in, x, y)
 
+print("Some interpolate points:")
 for i in range(50, 1001, 100):
-    print(x_in[i], y_in[i])
+    print((x_in[i], y_in[i]))
