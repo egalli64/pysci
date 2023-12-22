6874329adb8541375b59a2faa1693fc1cc48cf69
Emanuele <egalli64@gmail.com>
2023-12-22 09:07:16 +0100

    quad

---
+++ m2/s5_integrate/e1_quad.py
@@ -0,0 +1,19 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 2 - SciPy
+
+Integration - quad
+"""
+from scipy import integrate
+
+
+def parabola(x):
+    return x**2
+
+
+interval = (0, 1)
+result = integrate.quad(parabola, *interval)
+print("Result and error estimation:", result)
