a1025fecd53e9619df1a0719fdbe684f199f243b
Emanuele <egalli64@gmail.com>
2023-12-22 12:11:29 +0100

    review

--- m1/s5_concat_split/e2_split.py
+++ m1/s5_concat_split/e2_split.py
@@ -5,30 +5,31 @@ https://github.com/egalli64/pysci
 
 Module 1 - NumPy
 
-Array split
+Array - split
 """
 import numpy as np
 
 a = np.arange(9)
 print("An array", a)
 
-print("The array split in 3:", np.split(a, 3))
-print("Split at 2 and 7:", np.split(a, (2, 7)))
+print("A standard three-way split:", np.split(a, 3))
+print("A custom three-way split (at 2 and 7):", np.split(a, (2, 7)))
+print()
 
 m = np.arange(16).reshape((4, 4))
-print("A matrix:\n", m)
+print(f"A two-dimension array shaped {m.shape}:\n", m)
 
 above, below = np.split(m, 2, axis=0)
-print("Vertical split in two (axis 0):")
+print("Vertical (axis 0) split in two equal parts:")
 print(above, "is above")
 print(below, "is below")
 
 above, below = np.vsplit(m, 2)
-print("Vertical split in two (vsplit):")
+print("vsplit in two equal parts:")
 print(above, "is above")
 print(below, "is below")
 
 left, right = np.hsplit(m, 2)
-print("Horizontal split in two (hsplit):")
+print("\nhsplit in two equal parts:")
 print(left, "is left")
 print(right, "is right")
