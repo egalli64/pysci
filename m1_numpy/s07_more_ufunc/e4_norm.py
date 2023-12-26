f1ad442b56c867eb9b26ea23529876b3fdae90f0
Emanuele <egalli64@gmail.com>
2023-12-26 14:54:15 +0100

    matrix norm

--- m1_numpy/s07_more_ufunc/e4_norm.py
+++ m1_numpy/s07_more_ufunc/e4_norm.py
@@ -12,8 +12,15 @@ Norm
 import numpy as np
 from numpy import linalg as la
 
-a = np.arange(1, 4)
+a = np.arange(1, 5)
 print(f"The array a is {a}\n")
 
 print("Vector 2-norm (default) for a is", la.norm(a))
 print("Vector 1-norm for a is", la.norm(a, ord=1))
+print()
+
+m = a.reshape(2, 2)
+print(m, "m, a two-dimensional array\n")
+print("Matrix Forbenius norm ('fro' is the default) for m is", la.norm(m))
+print("Matrix 1-norm for m is", la.norm(m, ord=1))
+print("Matrix 2-norm for m is", la.norm(m, ord=2))
