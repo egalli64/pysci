d768bda4ae3979d80491a22c0fe13e63b908f405
Emanuele <egalli64@gmail.com>
2024-01-08 09:52:38 +0100

    more comments

--- m3_pandas/s2_series.py
+++ m3_pandas/s2_series.py
@@ -9,21 +9,26 @@ Series
 """
 import pandas as pd
 
+# from list to series
 ps = pd.Series([42, 99, 51, 27])
 print("A pandas series:", ps, sep="\n")
 print(f"\nIt is a {type(ps)} with dtype {ps.dtype}\n")
 print("The associated array is:\n", ps.array, "\n")
 print("The associated index is:", ps.index)
 
+# from two lists (values and indices) to series
 ps2 = pd.Series([24, 99, 15, 72], ["a", "b", "c", "d"])
 print("\nAnother pandas series:", ps2, sep="\n")
 print("\nThe associated index is:", ps2.index)
 
+# accessing an element
 print("\nElement 'b' is", ps2["b"])
 
+# extracting a slice
 ps3 = ps2[["c", "a", "d"]]
-print("\nA slice of ps2 (deepcopy):", ps3, sep="\n")
+print("\nA slice of ps2:", ps3, sep="\n")
 
+# operating a transformation
 ps4 = ps3 * 2
 print("\nDoubling ps3:", ps4, sep="\n")
 
