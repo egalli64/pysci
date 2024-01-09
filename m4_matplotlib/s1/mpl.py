2c9ee50237dc7027c0f07508ecb47b7273f9be2f
Emanuele <egalli64@gmail.com>
2024-01-09 11:24:39 +0100

    lib check

---
+++ m4_matplotlib/s1/mpl.py
@@ -0,0 +1,3 @@
+import matplotlib
+
+print(f"Using {matplotlib.__name__} {matplotlib.__version__}")
---
+++ m4_matplotlib/s1/sns.py
@@ -0,0 +1,5 @@
+import matplotlib
+import seaborn as sns
+
+print(f"Using {sns.__name__} {sns.__version__}", end=" ")
+print(f"on {matplotlib.__name__} {matplotlib.__version__}")
