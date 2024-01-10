91318d17b079c7b805567d8b36feb8719da04fc9
Emanuele <egalli64@gmail.com>
2024-01-10 21:53:16 +0100

    minor review

--- m4_matplotlib/s5_pie/e2_column.py
+++ m4_matplotlib/s5_pie/e2_column.py
@@ -5,15 +5,15 @@ https://github.com/egalli64/pysci
 
 Module 4 - Matplotlib
 
-Draw a simple pie chart
+Draw a simple pie chart from a DataFrame column
 Requires iris.csv from https://github.com/mwaskom/seaborn-data
 """
 import matplotlib.pyplot as plt
 import pandas as pd
 
 sample = pd.read_csv("data/iris.csv").sample(97)
-species_counts = sample["species"].value_counts()
-plt.pie(species_counts.values, labels=species_counts.index, autopct="%.2f%%")
+data = sample["species"].value_counts()
+plt.pie(data.values, labels=data.index, autopct="%.2f%%")
 
-plt.title("Pie Chart of Iris Species")
+plt.title("Pie Chart of Iris Species  in random sample")
 plt.show()
