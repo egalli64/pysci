724a4652ebee0350428404c0c3352c0a2e2dd2d4
Emanuele <egalli64@gmail.com>
2025-01-24 12:35:24 +0100

    review

--- m1_numpy/s02_array/ndarray_checker.py
+++ m1_numpy/s02_array/ndarray_checker.py
@@ -7,18 +7,21 @@ Module 1 - NumPy
 
 A utility function to check a NumPy array
 """
+
 import numpy as np
 
 
 def check(a: np.ndarray) -> None:
-    """Some basic info on the passed array"""
+    """Some basic info on the passed ndarray"""
     try:
-        print(f"{a} is a {a.ndim}-dimension {a.dtype} {type(a)}", end=" ")
-        print(f"with {a.size} elements,", end=" ")
+        print(a, "is ", end="")
+        print(f"a\n\t{a.ndim}-dimension {a.dtype} {type(a)} (type size {a.dtype.itemsize} byte)")
+        print(f"\twith {a.size} elements,", end=" ")
         print(f"shaped {a.shape}", end=" ")
         print(f"total byte size is {a.nbytes}")
     except AttributeError:
-        print(f"{a} is not an ndarray")
+        print("not an ndarray")
+
 
 if __name__ == "__main__":
     check(None)
