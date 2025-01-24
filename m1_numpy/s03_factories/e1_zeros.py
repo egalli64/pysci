332babab7a6a87ed2bd38a0bc2cd602dca7fe72e
Emanuele <egalli64@gmail.com>
2025-01-24 22:50:38 +0100

    split and clarify

---
+++ m1_numpy/s03_factories/e1_zeros.py
@@ -0,0 +1,17 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 1 - NumPy
+
+ndarray factory zeros()
+"""
+
+import numpy as np
+
+print("zeros:\n", np.zeros(3))
+
+print("\nzeros as int:\n", np.zeros(3, dtype=int))
+
+print("\nzeros, 2 rows, 3 columns:\n", np.zeros((2, 3)))
