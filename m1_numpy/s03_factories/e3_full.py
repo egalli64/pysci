e0406df3ac1988fc48df97cd0f8d09b818730bbd
Emanuele <egalli64@gmail.com>
2025-01-24 23:03:36 +0100

    split and clarify

---
+++ m1_numpy/s03_factories/e3_full.py
@@ -0,0 +1,15 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 1 - NumPy
+
+ndarray factory full()
+"""
+
+import numpy as np
+
+print("full of -1:\n", np.full(3, -1))
+
+print("\nfull of -1, 2 rows, 3 columns:\n", np.full((2, 3), -1))
