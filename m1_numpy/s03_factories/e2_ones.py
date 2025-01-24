849312d9a334fbfc9019d69762844e9cf227b9c4
Emanuele <egalli64@gmail.com>
2025-01-24 22:52:17 +0100

    split and clarify

---
+++ m1_numpy/s03_factories/e2_ones.py
@@ -0,0 +1,17 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 1 - NumPy
+
+ndarray factory ones()
+"""
+
+import numpy as np
+
+print("ones:\n", np.ones(3))
+
+print("\nones as int:\n", np.ones(3, dtype=int))
+
+print("\nones, 2 rows, 3 columns:\n", np.ones((2, 3)))
