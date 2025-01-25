7054032597eae97ac5d715c526a88dbe388d33cf
Emanuele <egalli64@gmail.com>
2025-01-25 13:53:45 +0100

    merged with empty() examples

--- m1_numpy/s03_factories/e3_full.py
+++ m1_numpy/s03_factories/e3_full.py
@@ -5,11 +5,15 @@ https://github.com/egalli64/pysci
 
 Module 1 - NumPy
 
-ndarray factory full()
+ndarray factories full() and empty()
 """
 
 import numpy as np
 
-print("full of -1:\n", np.full(3, -1))
+# full
+print("full of -1:", np.full(3, -1))
+print("\nfull of -1, 2 rows and 3 columns:\n", np.full((2, 3), -1))
 
-print("\nfull of -1, 2 rows, 3 columns:\n", np.full((2, 3), -1))
+# empty
+print("\nempty (uninitialized):", np.empty(3))
+print("\nnempty, 2 rows and 4 columns:\n", np.empty((2, 4), dtype=int))
