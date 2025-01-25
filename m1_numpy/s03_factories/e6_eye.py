efe7aec8d908fadf62a6eebd979348c92abb9e4c
Emanuele <egalli64@gmail.com>
2025-01-25 14:51:19 +0100

    split and clarify

---
+++ m1_numpy/s03_factories/e6_eye.py
@@ -0,0 +1,17 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 1 - NumPy
+
+ndarray factory eye()
+"""
+
+import numpy as np
+
+print("*** Identity matrix 3")
+print(np.eye(3))
+
+print("\n*** Identity matrix 3 (int)")
+print(np.eye(3, dtype=int))
