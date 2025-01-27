a91edc825738064ba1da368d1c902cd742ef0de1
Emanuele <egalli64@gmail.com>
2025-01-27 22:55:01 +0100

    split and clarify

---
+++ m1_numpy/s09_ufunc/e3_mult_div.py
@@ -0,0 +1,31 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 1 - NumPy
+
+Universal Functions: multiply(), divide(), floor_divide(), remainder(), mod()
+"""
+
+import numpy as np
+
+# an array with 6 values in [-6 ... 6]
+a = np.random.randint(-6, 7, size=6)
+# an array with 6 values, 3 negatives, 3 positive (no zero)
+b = np.concatenate((np.random.randint(-6, 0, size=3), np.random.randint(1, 7, size=3)))
+print("The arrays a and b are", a, b)
+
+print("a * b:", a * b)
+print("multiply() on a and b:", np.multiply(a, b))
+
+print("a / b:", a / b)
+print("divide() on a and b:", np.divide(a, b))
+
+print("a // b:", a // b)
+print("floor_divide() on a and b:", np.floor_divide(a, b))
+
+print("a % b:", a % b)
+print("remainder() on a and b:", np.remainder(a, b))
+# since NumPy 1.20 remainder() and mod() are synonyms
+print("mod() on a and b:", np.mod(a, b))
