c1e0e68ad68b362748569ee3f750de9093e7bb9c
Emanuele <egalli64@gmail.com>
2025-01-27 16:26:13 +0100

    clarify

--- m1_numpy/s08_split/e1D_split.py
+++ m1_numpy/s08_split/e1D_split.py
@@ -16,5 +16,10 @@ print("An array", a)
 # split in three equal subarray
 print("A standard three-way split:", np.split(a, 3))
 
+try:
+    np.split(a, 2)
+except ValueError as ex:
+    print("A split in two parts causes a ValueError: ", ex)
+
 # split in three subarray @ given index
 print("A custom three-way split (at index 2 and 7):", np.split(a, (2, 7)))
