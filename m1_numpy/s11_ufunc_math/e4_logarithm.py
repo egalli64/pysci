a88e3e4bc2bb82cd39340554be4d6fa6c07b734a
Emanuele <egalli64@gmail.com>
2025-01-28 20:02:35 +0100

    split and clarify

---
+++ m1_numpy/s11_ufunc_math/e4_logarithm.py
@@ -0,0 +1,20 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 1 - NumPy
+
+More Universal Functions
+
+Logarithmic
+"""
+
+import numpy as np
+
+xs = np.array((np.e, 2, 10))
+print(f"xs is {xs}\n")
+
+print("ln    xs:", np.log(xs))
+print("log2  xs:", np.log2(xs))
+print("log10 xs:", np.log10(xs))
