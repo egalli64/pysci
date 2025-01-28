5cda850cc24bdebede8f87c0512d42b00060151b
Emanuele <egalli64@gmail.com>
2025-01-28 19:03:57 +0100

    numpy consts

---
+++ m1_numpy/s11_ufunc_math/e1_constants.py
@@ -0,0 +1,19 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 1 - NumPy
+
+Math Universal Functions
+
+Constants
+"""
+
+import numpy as np
+
+xs = np.arange(1, 5)
+print(f"xs is {xs}, pi is {np.pi}, e is {np.e}\n")
+
+print("pi * xs =", np.pi * xs)
+print("e * xs = ", np.e * xs)
