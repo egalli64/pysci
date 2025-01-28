ec6b54cab29a11358576f237a7b8464a97b065f8
Emanuele <egalli64@gmail.com>
2025-01-28 15:30:40 +0100

    review

--- m1_numpy/s09_ufunc/e5_out.py
+++ m1_numpy/s09_ufunc/e5_out.py
@@ -9,20 +9,23 @@ Universal Functions
 
 The "out" parameter
 """
+
 import numpy as np
 
 array = np.arange(1, 7)
-target = np.empty(6)
 print("Input:", array)
+
+target = np.empty(6, np.int_)
+print("Target:", array)
 
 # sending output to the specified sink
 np.multiply(array, 10, out=target)
-print("Target:", target)
+print("Target after multiply input by 10:", target)
 
 xs = array[:3]
-print("Input (view):", xs)
+print("A view on input:", xs)
 # the sink could be a view, too
 sink = target[::2]
-print("Before changing the sink (view):", sink)
+print("A view on target:", sink)
 np.power(2, xs, out=sink)
-print("After changing sink and target:", sink, target)
+print("After sending output on the sink, the target changes too:", sink, target)
