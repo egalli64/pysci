80fd1d90ef1e659d191e9f29b5c5bc13c0ee8b03
Emanuele <egalli64@gmail.com>
2025-01-29 11:22:44 +0100

    review

--- m1_numpy/s12_ufunc_linalg/e4_linalg_inv.py
+++ m1_numpy/s12_ufunc_linalg/e4_linalg_inv.py
@@ -3,10 +3,11 @@ Python and Science
 
 https://github.com/egalli64/pysci
 
-Module 1 - NumPy
+Linear algebra
 
 linalg - inverse
 """
+
 import numpy as np
 from numpy import linalg as la
 
@@ -14,12 +15,12 @@ m = np.array([[1, 3, 5], [2, 5, 1], [2, 3, 8]])
 print(m, "is m\n")
 
 inv_m = la.inv(m)
-print(inv_m, "is m ^ -1 (inverse)\n")
+print(inv_m, "is the m inverse\n")
 
 # proof that it works
 m_id = m @ inv_m
-print(m_id, "is m @ m^-1\n")
-print("Is m @ m^-1 close enough to the identity matrix?", np.allclose(m_id, np.eye(3)))
+print(m_id, "is m @ inv(m)\n")
+print("Is m @ inv(m) close to the identity matrix?", np.allclose(m_id, np.eye(3)))
 
 print("\nOne-dimensional array has no inverse, LinAlgError is raised")
 try:
