93911aea93edce0363540b2c5cb34f65d557f884
Emanuele <egalli64@gmail.com>
2025-01-30 11:24:06 +0100

    version check

---
+++ m2_scipy/s1_setup/hello_scipy.py
@@ -0,0 +1,15 @@
+"""
+Python and Science
+
+https://github.com/egalli64/pysci
+
+Module 2 - SciPy
+
+Hello NumPy - SciPy
+"""
+
+import numpy as np
+import scipy
+
+print("Hello NumPy version", np.__version__)
+print("Hello SciPy version", scipy.__version__)
